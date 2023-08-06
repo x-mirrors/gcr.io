82c9a5db452ff6c9a72c86a65c7a2677d1178d6b
xiexianbin <me@xiexianbin.cn>
2023-08-06 12:49:56 +0800

    update knative source [skip ci]
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

--- tools/render-knative.sh
+++ tools/render-knative.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x
 for i in $(gcloud container images list --repository gcr.io/knative-releases/knative.dev | grep -v -i name); do
   gcloud container images list --repository $i/cmd | grep $i;
 done > ../gcr.io/knative-releases.txt
