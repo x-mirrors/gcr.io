79a2a1c210078bfd34f72890b009c13dbe8c67c4
xiexianbin <me@xiexianbin.cn>
2023-08-06 12:23:52 +0800

    update tekton source #12 [skip ci]
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

--- tools/render-tekton.sh
+++ tools/render-tekton.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -x
 for i in $(gcloud container images list --repository gcr.io/tekton-releases/github.com/tektoncd | grep -v -i name); do
   gcloud container images list --repository $i/cmd | grep $i;
 done > ../gcr.io/tekton-releases.txt
